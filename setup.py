6825e579d50a37b4e73bc40f70d9ea4fa0b0e2fb
Ketson R. M. dos Santos <47835396+ketsonroberto@users.noreply.github.com>
2020-12-11 18:38:39 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='PyCrack',
-    version="0.0.4",
+    version="0.0.5",
     url='https://github.com/eesd-epfl/PyCrack',
     description="PyCrack is a Python toolbox for analysis and quantification of cracks structural systems using optical methods.",
     author="Ketson R. M. dos Santos",
@@ -15,7 +15,7 @@ setup(
     package_dir={"": "src"},
     package_data={"": ["*.pdf"]},
     install_requires=[
-        "numpy", "scipy", "matplotlib", "scikit-learn", "scikit-image", "os"
+        "numpy", "scipy", "matplotlib", "scikit-learn", "scikit-image"
     ],
     classifiers=[
         'Programming Language :: Python :: 3',
