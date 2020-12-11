397e73323aa15f76c80f93c5f7e97bf34ee20a14
Ketson R. M. dos Santos <47835396+ketsonroberto@users.noreply.github.com>
2020-12-11 16:22:38 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,25 +3,24 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='UQpy',
-    version="3.0.1",
-    url='https://github.com/SURGroup/UQpy',
-    description="UQpy is a general purpose toolbox for Uncertainty Quantification",
-    author="Michael D. Shields, Dimitris G. Giovanis, Audrey Olivier, Aakash Bangalore-Satish, Mohit Chauhan, "
-           "Lohit Vandanapu, Ketson R.M. dos Santos",
-    author_email="UQpy.info@gmail.com",
+    name='pyCrack',
+    version="0.0.1",
+    url='https://github.com/eesd-epfl/PyCrack',
+    description="PyCrack is a Python toolbox for analysis and quantification of cracks structural systems using optical methods.",
+    author="Ketson R. M. dos Santos",
+    author_email="ketson.santos@epfl.ch",
     license='MIT',
     platforms=["OSX", "Windows", "Linux"],
     packages=find_packages("src"),
     package_dir={"": "src"},
     package_data={"": ["*.pdf"]},
     install_requires=[
-        "numpy", "scipy", "matplotlib", "scikit-learn", 'fire'
+        "numpy", "scipy", "matplotlib", "scikit-learn", "scikit-image", "os"
     ],
     classifiers=[
         'Programming Language :: Python :: 3',
         'Intended Audience :: Science/Research',
-        'Topic :: Scientific/Engineering :: Mathematics',
+        'Topic :: Scientific/Engineering :: Signal Processing',
         'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
     ],
